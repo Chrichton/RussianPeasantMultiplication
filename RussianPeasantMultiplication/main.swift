d0d8fbefa8957926ed4c4fff0d4add8140318c7e
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-02-28 18:07:52 +0100

    Initial Commit

---
+++ RussianPeasantMultiplication/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RussianPeasantMultiplication
+//
+//  Created by Heiko Goes on 28.02.20.
+//  Copyright © 2020 Heiko Goes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
