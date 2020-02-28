1e356d6195951cbdbb8e0dc4fcc5fbe276e0bbb0
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-02-28 18:48:36 +0100

    Initial

--- RussianPeasantMultiplication/main.swift
+++ RussianPeasantMultiplication/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+func getDividesOfTwo(_ number: Int) -> [Int] {
+    return getDividesOfTwoRecursive(number, lastNumbers: [number])
+}
 
+func getMultplesOfTwo(number: Int, length: Int) -> [Int] {
+    return (1..<length).reduce((number, [number])) { accu, _ in
+        let double = 2 * accu.0
+        return (double, accu.1 + [double])
+    }.1
+}
+
+func getDividesOfTwoRecursive(_ number: Int, lastNumbers: [Int]) -> [Int] {
+    let result = number / 2
+    
+    if result == 1 {
+        return lastNumbers + [1]
+    }
+    
+    return getDividesOfTwoRecursive(result, lastNumbers: lastNumbers + [result])
+}
+
+func multiply(_ arg1: Int, _ arg2: Int) -> Int {
+    let divides = getDividesOfTwo(arg1)
+    let tuples = zip(divides, getMultplesOfTwo(number: arg2, length: divides.count))
+    return tuples
+        .filter{ $0.0 % 2 != 0 }
+        .map{ $0.1 }
+        .reduce(0, +)
+}
+
+print(multiply(27, 82))
